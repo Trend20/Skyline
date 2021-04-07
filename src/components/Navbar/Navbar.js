528751f1a453969efe28dbb0936c0dfea80421d9
Trend20 <enockomondi305@gmail.com>
2021-04-07 11:12:34 +0300

    navbar commit

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 
 import logo from '../../assets/logo.png';
+
+// react icons
+import {FaFacebookF, FaGooglePlusG, FaTwitter} from 'react-icons/fa';
 import './Navbar.css';
 
 export default class Navbar extends Component {
     render() {
         return (
+            <div className="hero">
             <div className="navbar">
                <div className="logo">
                    <img src={logo} alt="logo"/>
@@ -20,6 +24,21 @@ export default class Navbar extends Component {
                        <li><a href="#">Contact</a></li>
                    </ul>
                </nav>
+            </div>
+
+            <div className="hero-description">
+                <h3>Skyline - The Best <b> MotoCMS Template</b></h3>
+                <p>Create and edit your business website with our responsive website builder.</p>
+                <button className="sub-btn">Subscribe</button>
+
+                <h1>Great</h1>
+            </div>
+
+            <div className="social-media">
+                <FaFacebookF />
+                <FaGooglePlusG />
+                <FaTwitter />
+            </div>
             </div>
         )
     }
