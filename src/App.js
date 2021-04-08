51967c52f25f0852e141d047a40eee7fa229388b
Trend20 <enockomondi305@gmail.com>
2021-04-08 05:38:42 +0300

    team commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Description from './components/Description/Description';
 import Features from './components/Features/Features';
 import Header from './components/Header/Header';
+import Team from './components/Team/Team';
 import Theme from './components/Theme/Theme';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
      <Description />
      <Features />
      <Theme />
+     <Team />
     </div>
   );
 }
---
+++ src/components/Team/Team.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { FaFacebookF, FaGooglePlusG, FaTwitter } from 'react-icons/fa'
+
+import './Team.css';
+
+function Team() {
+    return (
+        <div className="team">
+            <h1>Team</h1>
+
+            <div className="team-members">
+                <div className="member">
+                    <h3>Nicholas Bell</h3>
+                    <p>Founder & CEO</p>
+
+                    <div className="team-links">
+                        <i><FaFacebookF /></i>
+                        <i><FaTwitter /></i>
+                        <i><FaGooglePlusG /></i>
+                    </div>
+                    <img src="../../img/team1.png" alt="nick"/>
+                </div>
+
+                <div className="member">
+                    <h3>Gregory Ramos</h3>
+                    <p>Head of Development</p>
+
+                    <div className="team-links">
+                        <i><FaFacebookF /></i>
+                        <i><FaTwitter /></i>
+                        <i><FaGooglePlusG /></i>
+                    </div>
+                    <img src="../../img/team2.png" alt="ramos"/>
+                </div>
+
+                <div className="member">
+                    <h3>Craig Dean</h3>
+                    <p>Marketing Director</p>
+
+                    <div className="team-links">
+                        <i><FaFacebookF /></i>
+                        <i><FaTwitter /></i>
+                        <i><FaGooglePlusG /></i>
+                    </div>
+                    <img src="../../img/team3.png" alt="dean"/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Team
