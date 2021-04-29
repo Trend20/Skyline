9c5b6b5cd8f5a63e2fc9c9d874aae0e90954087a
Trend20 <enockomondi305@gmail.com>
2021-04-29 07:39:00 +0300

    restructuring the header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Contact from './components/Contact/Contact';
 import Description from './components/Description/Description';
 import Features from './components/Features/Features';
 import Footer from './components/Footer/Footer';
-import Header from './components/Header/Header';
+import Hero from './components/Hero/Hero';
 import Team from './components/Team/Team';
 import Testimonials from './components/Testimonials/Testimonials';
 import Theme from './components/Theme/Theme';
@@ -14,22 +14,22 @@ import Timer from './components/Timer/Timer';
 import Travel from './components/Travel/Travel';
 
 function App() {
-  return (
-    <div className="App">
-     <Header />
-     <Description />
-     <Features />
-     <Theme />
-     <Team />
-     <Construction />
-     <Testimonials />
-     <Travel />
-     <Timer />
-     <Clients />
-     <Contact />
-     <Footer />
-    </div>
-  );
+	return (
+		<div className="app">
+			<Hero />
+			<Description />
+			<Features />
+			<Theme />
+			<Team />
+			<Construction />
+			<Testimonials />
+			<Travel />
+			<Timer />
+			<Clients />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function Hero() {
+	return (
+		<div className="hero">
+			<div className="hero-text">
+				<div className="hero">
+					<header>
+						<div className="logo">
+							<img src="logo.png" alt="logo" />
+						</div>
+						<nav>
+							<ul>
+								<li>
+									<a href="#">Home</a>
+								</li>
+								<li>
+									<a href="#">About Us</a>
+								</li>
+								<li>
+									<a href="#">Pages</a>
+								</li>
+								<li>
+									<a href="#">Blog</a>
+								</li>
+								<li>
+									<a href="#">Contact</a>
+								</li>
+							</ul>
+						</nav>
+					</header>
+				</div>
+			</div>
+		</div>
+	);
+}
+
+export default Hero;
